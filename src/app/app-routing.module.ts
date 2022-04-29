dccc9cfa7222791be62f17731813d738dc663b9b
FelixMilan <felix.m.p.milan@gmail.com>
2022-04-29 17:41:11 +0100

    game updated in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HelpComponent } from './help/help.component';
+import { HomeComponent } from './home/home.component';
+import { MediaComponent } from './media/media.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'help', component: HelpComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'media', component: MediaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
